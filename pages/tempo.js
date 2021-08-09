95e5b71dcf08b8fd8b8c0d253ac53e33828399f3
Lucas <joselucasrs@gmail.com>
2021-08-09 14:36:13 -0300

    tentando forçar o deploy

--- pages/tempo.js
+++ pages/tempo.js
@@ -14,6 +14,7 @@ export function getStaticProps() {
   const staticDate = new Date();
   const staticDateString = staticDate.toGMTString();
 
+  // Forçar deploy
   return {
     props: {
       staticDateString,
